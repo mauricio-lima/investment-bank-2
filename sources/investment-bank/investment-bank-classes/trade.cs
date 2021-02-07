cb48006169251dfc4048172590e5575b89921b74
Mauricio Lima <github@mauriciolima.eti.br>
2021-02-07 14:17:55 -0300

    Introducing some methods of Trade class - Introducing getter and setter for Value property - Introducing method UpdateCategory with sample only for compiling after introduction of Value property set

--- sources/investment-bank/investment-bank-classes/trade.cs
+++ sources/investment-bank/investment-bank-classes/trade.cs
@@ -6,9 +6,33 @@ namespace investment_bank
 {
 	public class Trade : ITrade
 	{
-		public double	Value			{ get; }
+		private double mValue;
+		private string mCategory;
+
+		void UpdateCategory()
+		{
+			this.mCategory = "DEFAULTED";
+		}
+		public double Value
+		{
+			get => mValue;
+			set
+			{
+				this.mValue = value;
+				this.UpdateCategory();
+			}
+		}
 		public string	ClientSector	{ get; }
 		public DateTime NextPaymentDate	{ get; }
-		public string	Category		{ get; }
+		public string	Category		
+		{ 
+			get => this.mCategory; 
+		}
+
+		public Trade()
+		{
+			this.mCategory = "DEFAULTED";
+		}
 	}
 }
+
