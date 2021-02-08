dc70865cf9e4ad4029ff9b004ff81eca4847dda3
Mauricio Lima <github@mauriciolima.eti.br>
2021-02-08 01:55:07 -0300

    Change name of field from 'date' to 'ReferenceDate'

--- sources/investment-bank/q1/input.cs
+++ sources/investment-bank/q1/input.cs
@@ -6,8 +6,8 @@ namespace investment_bank
 {
 	public class Input
 	{
-		public DateTime date;
-		public List<ITrade> trades { get; private set; }
+		public DateTime     ReferenceDate	{ get; set; }
+		public List<ITrade> trades			{ get; private set; }
 
 		public Input()
 		{
